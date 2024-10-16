e47b4403ba50a304f617b5a7e8bbbbf2dc12a188
modengann <99417989+modengann@users.noreply.github.com>
2024-10-16 10:04:20 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class App {
+    //Write code that will:
+    //1. Store the following values in an ArrayList: 2, 1, 12, 25
+    //   The name of the ArrayList is up to you.
+    //2. Print the contents of your ArrayList using loops. This should work even if 
+    //   I add more elements to your ArrayList. For example, I might add 7 to the
+    //   ArrayList and this should still work.
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
